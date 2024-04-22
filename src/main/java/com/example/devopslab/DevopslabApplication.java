03de9fc7c0c4afbe3a4bcd98462cd69c4230fd8a
yeehoe <128334510+yeehoe@users.noreply.github.com>
2024-04-23 02:11:10 +0800

    init

---
+++ src/main/java/com/example/devopslab/DevopslabApplication.java
@@ -0,0 +1,13 @@
+package com.example.devopslab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopslabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopslabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devopslab/DevopslabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopslab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopslabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
